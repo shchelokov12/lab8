2f243013b791f4627117f1ab26b21fe77f9cc230
shchelokov12 <aesch8877@gmail.com>
2026-06-29 21:52:39 +0300

    chore: add .gitignore to exclude build artifacts

--- _builds/CMakeFiles/test_print.dir/mock_test.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_print.dir/mock_test.cpp.o: \
- /home/parallels/shchelokov12/workspace/projects/lab07/_builds/mock_test.cpp \
- /usr/include/stdc-predef.h
--- _builds/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/shchelokov12/workspace/projects/lab07
-# Build directory: /home/parallels/shchelokov12/workspace/projects/lab07/_builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(print_test "/home/parallels/shchelokov12/workspace/projects/lab07/_builds/test_print")
-set_tests_properties(print_test PROPERTIES  _BACKTRACE_TRIPLES "/home/parallels/shchelokov12/workspace/projects/lab07/CMakeLists.txt;37;add_test;/home/parallels/shchelokov12/workspace/projects/lab07/CMakeLists.txt;0;")
